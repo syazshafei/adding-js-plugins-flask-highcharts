f4ca209989f3c62a2ebf5ccb103ed0c7e60ca86c
Syazwan Shafei <syazwan567@gmail.com>
2018-12-27 09:00:23 +0800

    Edited

--- app.py
+++ app.py
@@ -12,5 +12,16 @@ def index(chartID = 'chart_ID', chart_type = 'bar', chart_height = 350):
 	yAxis = {"title": {"text": 'yAxis Label'}}
 	return render_template('index.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
+
+@app.route('/test2')
+def test2(chartID = 'test2', chart_type = 'column', chart_height = 350):
+	chart = {"renderTo": chartID, "type": chart_type, "height": chart_height}
+	title = {"text": 'Browser market shares. January, 2018'}
+	xAxis = {"type": 'category'}
+	yAxis = {"title": {"text": 'Total percent market share'}}
+	series = [{"name": "Browsers", "colorByPoint": 'true', "data": [{"name": "Chrome", "y": 62.74, "drilldown": "Chrome"}]}]
+	drilldown = {"series":[{"name": 'Chrome', "id": 'Chrome', "data": [["v65.0", 0.1], ["v64.0", 1.3]]}]}
+	return render_template('test2.html', chartID=chartID, chart=chart, title=title, xAxis=xAxis, yAxis=yAxis, series=series, drilldown=drilldown)
+
 if __name__ == "__main__":
-	app.run(debug = True, host='0.0.0.0', port=8080, passthrough_errors=True)
+	app.run(debug = True, host='0.0.0.0', port=8081)
---
+++ static/js/main2.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$(test2).highcharts({
+		chart: chart,
+		title: title,
+		xAxis: xAxis,
+		yAxis: yAxis,
+		series: series,
+		drilldown: drilldown
+	});
+});
